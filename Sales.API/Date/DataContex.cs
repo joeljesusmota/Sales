1f6cdebc3be5071e6b13bf499a2956c85e7ae042
joeljesusmota <joeljesusmot@gmail.com>
2023-05-04 09:42:51 -0400

    Add data context

---
+++ Sales.API/Date/DataContex.cs
@@ -0,0 +1,22 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Sales.Shared;
+using System.Data.Common;
+
+namespace Sales.API.Date
+{
+    public class DataContex : DbContext
+    {
+        public DataContex(DbContextOptions<DataContex> options) : base(options)
+        {
+
+        }
+
+        public DbSet<Country> Countries { get; set; }
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            base.OnModelCreating(modelBuilder);
+            modelBuilder.Entity<Country>().HasIndex(x => x.Name).IsUnique();
+        }
+    }
+}
--- Sales.API/Program.cs
+++ Sales.API/Program.cs
@@ -1,3 +1,8 @@
+using Microsoft.EntityFrameworkCore.SqlServer;
+using Microsoft.EntityFrameworkCore;
+//using Microsoft.EntityFrameworkCore.Tools;
+using Sales.API.Date;
+
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
@@ -6,6 +11,7 @@ builder.Services.AddControllers();
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
+builder.Services.AddDbContext<DataContex>(x => x.UseSqlServer("name=ConectionDB"));
 
 var app = builder.Build();
 
