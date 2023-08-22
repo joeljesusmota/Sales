93503017ddfe80ba3d0319bab4b4e98e45a5712e
joeljesusmota <joeljesusmot@gmail.com>
2023-08-22 16:23:40 -0400

    Seed Db V1

---
+++ Sales.API/Data/SeedDb.cs
@@ -0,0 +1,30 @@
+﻿using Sales.Shared;
+
+namespace Sales.API.Data
+{
+	public class SeedDb
+	{
+		private readonly DataContext _context;
+		public SeedDb(DataContext context)
+		{
+			this._context = context;
+		}
+
+		public async Task SeedAsync()
+		{
+			await _context.Database.EnsureCreatedAsync();
+			await CheckCountriesAsync();
+		}
+
+		private async Task CheckCountriesAsync()
+		{
+			if (!_context.Countries.Any())
+			{
+				_context.Countries.Add(new Country { Name = "Colombia" });
+				_context.Countries.Add(new Country { Name = "Rep. Dom." });
+				_context.Countries.Add(new Country { Name = "Usa" });
+				await _context.SaveChangesAsync();
+			}
+		}
+	}
+}
